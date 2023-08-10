969f7850f5dfb65ccdbcf69d4a138d039c83a087
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-08-09 22:43:47 -0700

    ADD UUID to corrrect ID issue

--- notes.js
+++ notes.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
-const path = require('path')
-const Route = require('express').Router()
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const Route = require('express').Router();
 
 // Get all notes
 Route.get("/api/notes", (req, res) => {
   const data = fs.readFileSync("db/db.json");
-  console.log(data)
   const notes = JSON.parse(data);
   res.json(notes);
 });
@@ -15,7 +15,7 @@ Route.post("/api/notes", (req, res) => {
   const data = fs.readFileSync("db/db.json");
   const notes = JSON.parse(data);
   const newNote = {
-    id: notes.length + 1,
+    id: uuidv4(),  // Use uuid to generate a unique ID
     title: req.body.title,
     text: req.body.text
   };
@@ -24,4 +24,21 @@ Route.post("/api/notes", (req, res) => {
   res.json(newNote);
 });
 
-module.exports = Route
+// Delete a note by ID
+Route.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+  const data = fs.readFileSync("db/db.json");
+  const notes = JSON.parse(data);
+  const newNotes = notes.filter(note => note.id !== noteId);
+
+  if (notes.length !== newNotes.length) {
+    fs.writeFileSync("db/db.json", JSON.stringify(newNotes));
+    res.json({message: 'Note deleted successfully.'});
+  } else {
+    res.status(404).json({message: 'Note not found.'});
+  }
+});
+
+module.exports = Route;
+
+
